da05e4942a500bb064d796f23c2452ffaccd956c
Zolender <ndeingare@gmail.com>
2024-11-03 12:13:15 +0200

    ...

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='card'>
+      <img className='card-img' src="https://via.placeholder.com/150" alt="A placeholder image" />
+      <h1 className='card-title'>Zolender</h1>
+      <p className='card-desc'>A first year student learning react for fun</p>
+    </div>
+  )
+}
+
+export default App
